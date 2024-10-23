be87b5fb0c9d70199d308805c4954ad913b60941
kundan kumar <114884243+kundan234432@users.noreply.github.com>
2024-10-23 19:13:13 +0000

    conversion of datatype

---
+++ Javascript/03_conversionOperation.js
@@ -0,0 +1,24 @@
+//all datatype are class based so while declaring use Capital letter for first alphabet like 
+//Number String
+
+let score = "33abc"
+let valInNumber = Number(score);
+console.log(typeof(score));
+console.log(typeof valInNumber);
+console.log(valInNumber);
+/*
+output 
+string
+number
+NaN
+
+*/
+
+/*
+"33" => 33
+"33abc" => NaN
+ true => 1; false =>0
+ "" => false
+ "hitesh" => true
+
+*/
