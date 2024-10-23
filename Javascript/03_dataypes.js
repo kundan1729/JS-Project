608ddd18b4123f4416b209d135bfa4dda075f44b
kundan kumar <114884243+kundan234432@users.noreply.github.com>
2024-10-23 18:53:31 +0000

    "Datatype in javascript"

---
+++ Javascript/03_dataypes.js
@@ -0,0 +1,26 @@
+"use script" // treat all JS code as newer version
+//alert(3 + 3) // we are using node js not browser
+
+//code redability should be high
+
+ //####  Primitive datatype
+
+// number => 2 to power 53
+//bigint
+//string => ""
+//boolen => true/false
+//null => standalone value
+//undefined =>
+// symbol =>unique  (used while we use react )
+
+
+// object
+
+
+//typeof
+/*
+  typeof null is object
+  typeof undefine is undefine
+
+
+*/
